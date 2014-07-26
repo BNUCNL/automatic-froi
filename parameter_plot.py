e77c102735a125067d5da1599290881ec62e0659
Lijie Huang <huanglijie.seal@gmail.com>
2014-07-26 17:24:14 +0800

    update

--- parameter_plot.py
+++ parameter_plot.py
@@ -5,8 +5,10 @@ import os
 import numpy as np
 import pylab as pl
 
-data_dir = r'/nfs/h1/workingshop/huanglijie/autoroi/data'
-data_file = os.path.join(data_dir, 'parameter_score.npz')
+src_dir = r'/nfs/h1/workingshop/huanglijie/autoroi'
+data_dir = os.path.join(src_dir, 'data', 'cv')
+
+data_file = os.path.join(data_dir, 'cv_0', 'parameter_cv_data.npz')
 data = np.load(data_file)
 cv_score = data['cv_score']
 oob_score = data['oob_score']
@@ -15,32 +17,32 @@ ofa_dice = data['ofa_dice']
 
 # row: n_tree, colume: depth
 fig = pl.figure(figsize=(10, 10))
-ntree_idx = np.arange(0, 70, 10)
-depth_idx = np.arange(0, 70, 10)
+ntree_idx = np.arange(0, 70, 5)
+depth_idx = np.arange(0, 70, 5)
 # cv_score
 pl.subplot(2, 2, 1)
-pl.pcolor(ntree_idx, depth_idx, cv_score)
+pl.pcolor(ntree_idx, depth_idx, np.mean(cv_score, axis=2))
 pl.colorbar()
 pl.xlabel('depth')
 pl.ylabel('# of trees')
 pl.title('CV score')
 # oob_score
 pl.subplot(2, 2, 2)
-pl.pcolor(ntree_idx, depth_idx, oob_score)
+pl.pcolor(ntree_idx, depth_idx, np.mean(oob_score, axis=2))
 pl.colorbar()
 pl.xlabel('depth')
 pl.ylabel('# of trees')
 pl.title('OOB score')
 # ffa_dice
 pl.subplot(2, 2, 3)
-pl.pcolor(ntree_idx, depth_idx, ffa_dice)
+pl.pcolor(ntree_idx, depth_idx, np.mean(ffa_dice, axis=2))
 pl.colorbar()
 pl.xlabel('depth')
 pl.ylabel('# of trees')
 pl.title('FFA Dice')
 # ffa_dice
 pl.subplot(2, 2, 4)
-pl.pcolor(ntree_idx, depth_idx, ofa_dice)
+pl.pcolor(ntree_idx, depth_idx, np.mean(ofa_dice, axis=2))
 pl.colorbar()
 pl.xlabel('depth')
 pl.ylabel('# of trees')
