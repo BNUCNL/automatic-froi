9336504d7bf111aea5a312e215c0de094d837817
Lijie Huang <huanglijie.seal@gmail.com>
2014-07-27 21:11:23 +0800

    update

--- clf_bench.py
+++ clf_bench.py
@@ -30,7 +30,7 @@ sessid = [line.strip() for line in sessid]
 cv_num = 5
 
 # split all subjects into 5 folds
-subj_group = arlib.split_subject(sessid, cv_num)
+#subj_group = arlib.split_subject(sessid, cv_num)
 #arlib.save_subject_group(subj_group, data_dir)
 #
 ## data preparation for cross-validation
@@ -55,118 +55,194 @@ subj_group = arlib.split_subject(sessid, cv_num)
 #                                        out_dir=cv_dir), sessid)
 #    pool.terminate()
 
-# Cross-validation to evaluate the performance of the model
+##-- Cross-validation to select parameters
+## split all subjects into 5 folds
+#subj_group = arlib.split_subject(sessid, cv_num)
 #for i in range(cv_num):
-for i in [0]:
+#    print 'CV iter - ' + str(i)
+#    cv_dir = os.path.join(data_dir, 'cv_' + str(i))
+#    
+#    # split data into training and test group
+#    test_sessid = subj_group[i]
+#    # load test data
+#    test_data = arlib.get_list_data(test_sessid, cv_dir)
+#
+#    train_sessid = []
+#    for j in range(cv_num):
+#        if not j == i:
+#            train_sessid += subj_group[j]
+#    # split training data into n folds for inner CV
+#    inner_cv_group = arlib.split_subject(train_sessid, cv_num)
+#
+#    # parameters of random forest
+#    n_tree = range(5, 70, 5)
+#    depth = range(5, 70, 5)
+#    
+#    # output matrix
+#    oob_score = np.zeros((len(n_tree), len(depth), cv_num))
+#    cv_score = np.zeros((len(n_tree), len(depth), cv_num))
+#    ofa_dice = np.zeros((len(n_tree), len(depth), cv_num))
+#    ffa_dice = np.zeros((len(n_tree), len(depth), cv_num))
+#
+#    # inner CV for parameter selection
+#    for inner_i in range(cv_num):
+#        print 'Inner CV - %s'%(inner_i)
+#        # get sessid
+#        inner_test_sessid = inner_cv_group[inner_i]
+#        inner_train_sessid = []
+#        for inner_j in range(cv_num):
+#            if not inner_j == inner_i:
+#                inner_train_sessid += inner_cv_group[inner_j]
+#        
+#        print 'Load data ... '
+#        inner_train_data = arlib.get_list_data(inner_train_sessid, cv_dir)
+#        inner_test_data = arlib.get_list_data(inner_test_sessid, cv_dir)
+#        ## sample stats
+#        #print 'Samples stats for train-dataset:'
+#        #arlib.samples_stat(train_data)
+#        #print 'Samples stats for test-dataset:'
+#        #arlib.samples_stat(test_data)
+#
+#        # split label and feature
+#        train_x = inner_train_data[..., :-1]
+#        train_y = inner_train_data[..., -1]
+#        test_x = inner_test_data[..., :-1]
+#        test_y = inner_test_data[..., -1]
+#
+#        for t_idx in range(len(n_tree)):
+#            for d_idx in range(len(depth)):
+#                p = [n_tree[t_idx], depth[d_idx]]
+#                print 'Parameter: n_tree - %s; depth - %s'%(p[0], p[1])
+#                #-- compare OOB eror rate and the CV error rate
+#                # OOB error rate
+#                clf = RandomForestClassifier(n_estimators=p[0],
+#                                             criterion='gini',
+#                                             max_depth=p[1],
+#                                             n_jobs=20,
+#                                             oob_score=True)
+#                clf.fit(train_x, train_y)
+#                oob_score[t_idx, d_idx, inner_i] = clf.oob_score_
+#                print 'OOB score is %s'%(str(clf.oob_score_))
+#                # Cross-Validation
+#                clf = RandomForestClassifier(n_estimators=p[0],
+#                                             criterion='gini',
+#                                             max_depth=p[1],
+#                                             n_jobs=20)
+#                clf.fit(train_x, train_y)
+#                cv_score[t_idx, d_idx, inner_i] = clf.score(test_x, test_y)
+#                print 'Prediction score is %s'%(clf.score(test_x, test_y))
+#                print 'Dice coefficient:'
+#                pred_y = clf.predict(test_x)
+#                for label_idx in [1, 3]:
+#                    P = pred_y == label_idx
+#                    T = test_y == label_idx
+#                    dice_val = mymath.dice_coef(T, P)
+#                    print 'Dice for label %s: %f'%(label_idx, dice_val)
+#                    if label_idx == 3:
+#                        ffa_dice[t_idx, d_idx, inner_i] = dice_val
+#                    else:
+#                        ofa_dice[t_idx, d_idx, inner_i] = dice_val
+#                print '-----------------------'
+#
+#    out_data_file = os.path.join(cv_dir, 'parameter_cv_data.npz')
+#    np.savez(out_data_file, cv_score=cv_score, oob_score=oob_score,
+#             ffa_dice=ffa_dice, ofa_dice=ofa_dice)
+
+#-- Cross-validation to evaluate performance of model
+# parameters of random forest
+n_tree = range(10, 70, 10)
+depth = range(10, 70, 10)
+
+# output matrix
+oob_score = np.zeros((len(n_tree), len(depth), cv_num))
+cv_score = np.zeros((len(n_tree), len(depth), cv_num))
+ofa_dice = np.zeros((len(n_tree), len(depth), cv_num))
+ffa_dice = np.zeros((len(n_tree), len(depth), cv_num))
+
+# split all subjects into 5 folds
+subj_group = arlib.split_subject(sessid, cv_num)
+for i in range(cv_num):
     print 'CV iter - ' + str(i)
     cv_dir = os.path.join(data_dir, 'cv_' + str(i))
     
     # split data into training and test group
     test_sessid = subj_group[i]
-    # load test data
-    test_data = arlib.get_list_data(test_sessid, cv_dir)
-
     train_sessid = []
     for j in range(cv_num):
         if not j == i:
             train_sessid += subj_group[j]
-    # split training data into n folds for inner CV
-    inner_cv_group = arlib.split_subject(train_sessid, cv_num)
 
-    # parameters of random forest
-    n_tree = range(5, 70, 5)
-    depth = range(5, 70, 5)
+    # load test and train data
+    print 'Load data ...'
+    test_data = arlib.get_list_data(test_sessid, cv_dir)
+    train_data = arlib.get_list_data(train_sessid, cv_dir)
     
-    # output matrix
-    oob_score = np.zeros((len(n_tree), len(depth), cv_num))
-    cv_score = np.zeros((len(n_tree), len(depth), cv_num))
-    ofa_dice = np.zeros((len(n_tree), len(depth), cv_num))
-    ffa_dice = np.zeros((len(n_tree), len(depth), cv_num))
+    ## sample stats
+    #print 'Samples stats for train-dataset:'
+    #arlib.samples_stat(train_data)
+    #print 'Samples stats for test-dataset:'
+    #arlib.samples_stat(test_data)
 
-    # inner CV for parameter selection
-    for inner_i in range(cv_num):
-        print 'Inner CV - %s'%(inner_i)
-        # get sessid
-        inner_test_sessid = inner_cv_group[inner_i]
-        inner_train_sessid = []
-        for inner_j in range(cv_num):
-            if not inner_j == inner_i:
-                inner_train_sessid += inner_cv_group[inner_j]
-        
-        print 'Load data ... '
-        inner_train_data = arlib.get_list_data(inner_train_sessid, cv_dir)
-        inner_test_data = arlib.get_list_data(inner_test_sessid, cv_dir)
-        ## sample stats
-        #print 'Samples stats for train-dataset:'
-        #arlib.samples_stat(train_data)
-        #print 'Samples stats for test-dataset:'
-        #arlib.samples_stat(test_data)
+    # split label and feature
+    train_x = inner_train_data[..., :-1]
+    train_y = inner_train_data[..., -1]
+    test_x = inner_test_data[..., :-1]
+    test_y = inner_test_data[..., -1]
 
-        # split label and feature
-        train_x = inner_train_data[..., :-1]
-        train_y = inner_train_data[..., -1]
-        test_x = inner_test_data[..., :-1]
-        test_y = inner_test_data[..., -1]
+    for t_idx in range(len(n_tree)):
+        for d_idx in range(len(depth)):
+            p = [n_tree[t_idx], depth[d_idx]]
+            print 'Parameter: n_tree - %s; depth - %s'%(p[0], p[1])
+            #-- compare OOB eror rate and the CV error rate
+            # OOB error rate
+            clf = RandomForestClassifier(n_estimators=p[0],
+                                         criterion='gini',
+                                         max_depth=p[1],
+                                         n_jobs=20,
+                                         oob_score=True)
+            clf.fit(train_x, train_y)
+            oob_score[t_idx, d_idx, inner_i] = clf.oob_score_
+            print 'OOB score is %s'%(str(clf.oob_score_))
+            # cross-validation
+            cv_score[t_idx, d_idx, inner_i] = clf.score(test_x, test_y)
+            print 'Prediction score is %s'%(clf.score(test_x, test_y))
+            print 'Dice coefficient:'
+            pred_y = clf.predict(test_x)
+            for label_idx in [1, 3]:
+                P = pred_y == label_idx
+                T = test_y == label_idx
+                dice_val = mymath.dice_coef(T, P)
+                print 'Dice for label %s: %f'%(label_idx, dice_val)
+                if label_idx == 3:
+                    ffa_dice[t_idx, d_idx, inner_i] = dice_val
+                else:
+                    ofa_dice[t_idx, d_idx, inner_i] = dice_val
+            print '-----------------------'
 
-        for t_idx in range(len(n_tree)):
-            for d_idx in range(len(depth)):
-                p = [n_tree[t_idx], depth[d_idx]]
-                print 'Parameter: n_tree - %s; depth - %s'%(p[0], p[1])
-                #-- compare OOB eror rate and the CV error rate
-                # OOB error rate
-                clf = RandomForestClassifier(n_estimators=p[0],
-                                             criterion='gini',
-                                             max_depth=p[1],
-                                             n_jobs=20,
-                                             oob_score=True)
-                clf.fit(train_x, train_y)
-                oob_score[t_idx, d_idx, inner_i] = clf.oob_score_
-                print 'OOB score is %s'%(str(clf.oob_score_))
-                # Cross-Validation
-                clf = RandomForestClassifier(n_estimators=p[0],
-                                             criterion='gini',
-                                             max_depth=p[1],
-                                             n_jobs=20)
-                clf.fit(train_x, train_y)
-                cv_score[t_idx, d_idx, inner_i] = clf.score(test_x, test_y)
-                print 'Prediction score is %s'%(clf.score(test_x, test_y))
-                print 'Dice coefficient:'
-                pred_y = clf.predict(test_x)
-                for label_idx in [1, 3]:
-                    P = pred_y == label_idx
-                    T = test_y == label_idx
-                    dice_val = mymath.dice_coef(T, P)
-                    print 'Dice for label %s: %f'%(label_idx, dice_val)
-                    if label_idx == 3:
-                        ffa_dice[t_idx, d_idx, inner_i] = dice_val
-                    else:
-                        ofa_dice[t_idx, d_idx, inner_i] = dice_val
-                print '-----------------------'
+out_data_file = os.path.join(data_dir, 'parameter_cv_data.npz')
+np.savez(out_data_file, cv_score=cv_score, oob_score=oob_score,
+         ffa_dice=ffa_dice, ofa_dice=ofa_dice)
 
-    out_data_file = os.path.join(cv_dir, 'parameter_cv_data.npz')
-    np.savez(out_data_file, cv_scorei=cv_score, oob_score=oob_score,
-             ffa_dice=ffa_dice, ofa_dice=ofa_dice)
+##-- model validation
+## model defination
+#clf = RandomForestClassifier(n_estimators=50, max_depth=20,
+#                             criterion='entropy', max_features='sqrt',
+#                             bootstrap=True, n_jobs=10)
+## model training
+#tt = time.time()
+#clf.fit(train_x, train_y)
+#print 'Model training costs %s'%(time.time() - tt)
+#print 'Feature importance: ', clf.feature_importances_
 
-    ##-- model validation
-    ## model defination
-    #clf = RandomForestClassifier(n_estimators=50, max_depth=20,
-    #                             criterion='entropy', max_features='sqrt',
-    #                             bootstrap=True, n_jobs=10)
-    ## model training
-    #tt = time.time()
-    #clf.fit(train_x, train_y)
-    #print 'Model training costs %s'%(time.time() - tt)
-    #print 'Feature importance: ', clf.feature_importances_
+## model testing
+#predicted_y = clf.predict(test_x)
+## prob_predicted_y = clf.predict_proba(test_x)
 
-    ## model testing
-    #predicted_y = clf.predict(test_x)
-    ## prob_predicted_y = clf.predict_proba(test_x)
-
-    ## model evaluation
-    #for l in range(1, 6, 2):
-    #    print 'Dice coefficient for label ' + str(l),
-    #    true_bool = test_y == l
-    #    predicted_bool = predicted_y == l
-    #    dice_coef = arlib.dice(true_bool, predicted_bool)
-    #    print dice_coef
+## model evaluation
+#for l in range(1, 6, 2):
+#    print 'Dice coefficient for label ' + str(l),
+#    true_bool = test_y == l
+#    predicted_bool = predicted_y == l
+#    dice_coef = arlib.dice(true_bool, predicted_bool)
+#    print dice_coef
     
--- parameter_plot.py
+++ parameter_plot.py
@@ -3,7 +3,9 @@
 
 import os
 import numpy as np
-import pylab as pl
+import matplotlib.pyplot as pl
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
 
 src_dir = r'/nfs/h1/workingshop/huanglijie/autoroi'
 data_dir = os.path.join(src_dir, 'data', 'cv')
@@ -15,38 +17,73 @@ oob_score = data['oob_score']
 ffa_dice = data['ffa_dice']
 ofa_dice = data['ofa_dice']
 
-# row: n_tree, colume: depth
-fig = pl.figure(figsize=(10, 10))
-ntree_idx = np.arange(0, 70, 5)
-depth_idx = np.arange(0, 70, 5)
-# cv_score
-pl.subplot(2, 2, 1)
-pl.pcolor(ntree_idx, depth_idx, np.mean(cv_score, axis=2))
-pl.colorbar()
-pl.xlabel('depth')
-pl.ylabel('# of trees')
-pl.title('CV score')
-# oob_score
-pl.subplot(2, 2, 2)
-pl.pcolor(ntree_idx, depth_idx, np.mean(oob_score, axis=2))
-pl.colorbar()
-pl.xlabel('depth')
-pl.ylabel('# of trees')
-pl.title('OOB score')
-# ffa_dice
-pl.subplot(2, 2, 3)
-pl.pcolor(ntree_idx, depth_idx, np.mean(ffa_dice, axis=2))
-pl.colorbar()
-pl.xlabel('depth')
-pl.ylabel('# of trees')
-pl.title('FFA Dice')
-# ffa_dice
-pl.subplot(2, 2, 4)
-pl.pcolor(ntree_idx, depth_idx, np.mean(ofa_dice, axis=2))
-pl.colorbar()
-pl.xlabel('depth')
-pl.ylabel('# of trees')
-pl.title('OFA Dice')
+##-- 2d matrix
+## row: n_tree, colume: depth
+#fig = pl.figure(figsize=(10, 10))
+#ntree_idx = np.arange(0, 70, 5)
+#depth_idx = np.arange(0, 70, 5)
+## cv_score
+#pl.subplot(2, 2, 1)
+#pl.pcolor(ntree_idx, depth_idx, np.mean(cv_score, axis=2))
+#pl.colorbar()
+#pl.xlabel('depth')
+#pl.ylabel('# of trees')
+#pl.title('CV score')
+## oob_score
+#pl.subplot(2, 2, 2)
+#pl.pcolor(ntree_idx, depth_idx, np.mean(oob_score, axis=2))
+#pl.colorbar()
+#pl.xlabel('depth')
+#pl.ylabel('# of trees')
+#pl.title('OOB score')
+## ffa_dice
+#pl.subplot(2, 2, 3)
+#pl.pcolor(ntree_idx, depth_idx, np.mean(ffa_dice, axis=2))
+#pl.colorbar()
+#pl.xlabel('depth')
+#pl.ylabel('# of trees')
+#pl.title('FFA Dice')
+## ffa_dice
+#pl.subplot(2, 2, 4)
+#pl.pcolor(ntree_idx, depth_idx, np.mean(ofa_dice, axis=2))
+#pl.colorbar()
+#pl.xlabel('depth')
+#pl.ylabel('# of trees')
+#pl.title('OFA Dice')
+#pl.show()
 
+#-- 3d surface
+# row: n_tree, colume: depth
+fig = pl.figure()
+ax = fig.gca(projection='3d')
+ntree_idx, depth_idx = np.mgrid[5:70:5, 5:70:5]
+# cv_score
+surf = ax.plot_surface(ntree_idx, depth_idx, np.mean(cv_score, axis=2),
+                       cmap=cm.coolwarm, rstride=1, cstride=1)
+ax.set_xlabel('depth')
+ax.set_ylabel('# of trees')
+ax.set_zlabel('CV  score')
+fig.colorbar(surf, shrink=0.5, aspect=5)
+## oob_score
+#surf = ax.plot_surface(ntree_idx, depth_idx, np.mean(oob_score, axis=2),
+#                       cmap=cm.coolwarm, rstride=1, cstride=1)
+#ax.set_xlabel('depth')
+#ax.set_ylabel('# of trees')
+#ax.set_zlabel('OOB  score')
+#fig.colorbar(surf, shrink=0.5, aspect=5)
+## ffa_dice
+#surf = ax.plot_surface(ntree_idx, depth_idx, np.mean(ffa_dice, axis=2),
+#                       cmap=cm.coolwarm, rstride=1, cstride=1)
+#ax.set_xlabel('depth')
+#ax.set_ylabel('# of trees')
+#ax.set_zlabel('FFA Dice')
+#fig.colorbar(surf, shrink=0.5, aspect=5)
+## ofa_dice
+#surf = ax.plot_surface(ntree_idx, depth_idx, np.mean(ofa_dice, axis=2),
+#                       cmap=cm.coolwarm, rstride=1, cstride=1)
+#ax.set_xlabel('depth')
+#ax.set_ylabel('# of trees')
+#ax.set_zlabel('OFA Dice')
+#fig.colorbar(surf, shrink=0.5, aspect=5)
 pl.show()
 
